1acdff4f6c9f7c4b23580d02a79ec54dfb1129e0
sean0921 <sean.li.shin.ho@gmail.com>
2018-08-22 16:26:28 +0800

    refine indent and change dir. behavior

--- 99_runpkr_teqc.sh
+++ 99_runpkr_teqc.sh
@@ -3,12 +3,14 @@
 case "$1" in
 
 	dir)
-		for i in $(ls $2/*.T0[0-2])
-		do runpkr00 -g -d $i
-		done
-		for j in $(ls $2/*.tgd || ls $2/*.dat)
-		do teqc -tr d +obs + -tbin 1d $(echo $j|cut -b 1-4) $j
-		done
+		cd $2 && \
+			for i in $(ls *.T0[0-2])
+			do runpkr00 -g -d $i
+			done \
+		&& \
+			for j in $(ls *.tgd || ls *.dat)
+			do teqc -tr d +obs + -tbin 1d $(echo $j|cut -b 1-4) $j
+			done
 		;;
 
 	*)
