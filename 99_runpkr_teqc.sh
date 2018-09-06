ebc482f111b988a2a28cc13edec4192374d458fa
sean0921 <sean.li.shin.ho@gmail.com>
2018-09-06 11:12:28 +0800

    add script for 1s interval file

--- 99_runpkr_teqc.sh
+++ 99_runpkr_teqc.sh
@@ -12,6 +12,22 @@ case "$1" in
 			do teqc -tr d +obs + -tbin 1d $(echo $j|cut -b 1-4) $j
 			done
 		;;
+		
+	dirh)
+		cd $2 && \
+			for i in $(ls *.[Tt]0[0-2])
+			do runpkr00 -g -d $i
+			done \
+		&& \
+			for j in $(ls *.tgd *.dat)
+			do teqc -tr d +obs + -tbin 1h $(echo $j|cut -b 1-4) $j
+			done \
+		&& \
+			for k in $(ls *.1[1-9]o)
+			do teqc -phc $k >> $(echo $k|cut -b 1-7)1$(echo $k|cut -b 9-12)
+			done
+		;;
+
 
 	*)
 		echo "Usage: ${0} dir {directory name}"
